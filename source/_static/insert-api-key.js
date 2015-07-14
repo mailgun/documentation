063d88e6ccdcd31d1dbf342b3d7914e3a9328015
Eddy Hernandez <eddy.hernandez@rackspace.com>
2015-07-13 17:04:24 -0700

    check for features object

--- source/_static/insert-api-key.js
+++ source/_static/insert-api-key.js
@@ -1,22 +1,25 @@
 var replace_dom=function(user) {
 	var e=document.getElementsByTagName("*");
-	for (var i=0; i < e.length; i++) {
-		var children = e[i].childNodes;
-		for (var j=0;j<children.length; j++) {
-			if (children[j].nodeType == 3 &&
-				children[j].nodeValue.indexOf("YOUR_API_KEY") > -1) {
-				children[j].nodeValue = children[j].nodeValue.replace(
-					"YOUR_API_KEY", user.api_key);
-			}
-	                if (children[j].nodeType == 3 &&
-                                children[j].nodeValue.indexOf("YOU@YOUR_DOMAIN_NAME") > -1) {
-                                children[j].nodeValue = children[j].nodeValue.replace(
-                                        "YOU@YOUR_DOMAIN_NAME", user.email);
-			}
-			if (children[j].nodeType == 3 &&
-				children[j].nodeValue.indexOf("YOUR_DOMAIN_NAME") > -1) {
-				children[j].nodeValue = children[j].nodeValue.replace(
-					"YOUR_DOMAIN_NAME", user.active_domain);
+
+	if(user.features && !user.features.admin){
+		for (var i=0; i < e.length; i++) {
+			var children = e[i].childNodes;
+			for (var j=0;j<children.length; j++) {
+				if (children[j].nodeType == 3 &&
+					children[j].nodeValue.indexOf("YOUR_API_KEY") > -1) {
+					children[j].nodeValue = children[j].nodeValue.replace(
+						"YOUR_API_KEY", user.api_key);
+				}
+		                if (children[j].nodeType == 3 &&
+	                                children[j].nodeValue.indexOf("YOU@YOUR_DOMAIN_NAME") > -1) {
+	                                children[j].nodeValue = children[j].nodeValue.replace(
+	                                        "YOU@YOUR_DOMAIN_NAME", user.email);
+				}
+				if (children[j].nodeType == 3 &&
+					children[j].nodeValue.indexOf("YOUR_DOMAIN_NAME") > -1) {
+					children[j].nodeValue = children[j].nodeValue.replace(
+						"YOUR_DOMAIN_NAME", user.active_domain);
+				}
 			}
 		}
 	}
